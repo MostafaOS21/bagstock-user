9669d3d3a6c3a085bd193385c783207cf09d188a
MostafaOS21 <mostafaos2021fp@gmail.com>
2023-09-17 03:16:34 +0300

    first commit

--- src/app/profile/CartList.tsx
+++ src/app/profile/CartList.tsx
@@ -9,32 +9,31 @@ export default function CartList({ userId }: { userId: string }) {
   const router = useRouter();
   const [orders, setOrders] = useState<Order[]>([]);
 
-  const getCart = async () => {
-    const URL = process.env.NEXT_PUBLIC_BACK_END as string;
-    try {
-      const res = await fetch(`${URL}/users/cart/${userId}`, {
-        method: "GET",
-        credentials: "include",
-      });
-
-      const cart: { orders: Order[] } = await res.json();
-
-      if (res.ok) {
-        const currDate = new Date();
-        setOrders(cart.orders);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const getCart = async () => {
+      const URL = process.env.NEXT_PUBLIC_BACK_END as string;
+      try {
+        const res = await fetch(`${URL}/users/cart/${userId}`, {
+          method: "GET",
+          credentials: "include",
+        });
+
+        const cart: { orders: Order[] } = await res.json();
+
+        if (res.ok) {
+          const currDate = new Date();
+          setOrders(cart.orders);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getCart();
   }, []);
 
   useEffect(() => {
     router.push("/signin");
-  }, [userData.authed]);
+  }, [userData.authed, router]);
 
   return (
     <div className="px-6 py-14">
